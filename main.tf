0b714a8f25400c84383ae868109d0447a3d148b4
Ian Rodrigues <ianrdgs@gmail.com>
2020-06-09 15:30:05 -0300

    feat: remove unused locals

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
 terraform {
   required_version = "~> 0.12"
 }
-
-locals {
-  name_prefix = "${var.name}-${var.environment_short}"
-
-  tags = {
-    Application = var.name
-    Environment = var.environment
-    Terraform   = "yes"
-  }
-}
