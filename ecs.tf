efad19a219e392ca28c378dc153bf1a868eb4e8b
Ian Rodrigues <ianrdgs@gmail.com>
2020-06-28 16:05:59 -0300

    fix: remove create_before_destroy from ecs service

--- ecs.tf
+++ ecs.tf
@@ -44,7 +44,6 @@ resource "aws_ecs_service" "this" {
   tags = var.tags
 
   lifecycle {
-    create_before_destroy = true
-    ignore_changes        = [desired_count]
+    ignore_changes = [desired_count]
   }
 }
